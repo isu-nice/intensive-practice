f888df9b826215e972bda87010153383a59128da
Isu Shin <isu010614@naver.com>
2023-02-27 13:50:06 +0900

    feat: 제품 출고, 입고 기능 구현

--- src/project/sixth/MyStorage.java
+++ src/project/sixth/MyStorage.java
@@ -5,9 +5,8 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MyStorage {
-    private static final String VERSION = "[Item_Storage_V3]";
-    private static final String SYSTEM = "[System]";
-    private static final String CLEAR = "[Clear]";
+    private static final String SYSTEM = "[System] ";
+    private static final String CLEAR = "[Clear] ";
     private static final String CONTOUR = "-".repeat(30);
     private static final int REGISTER_QUANTITY = 5;
 
@@ -17,7 +16,7 @@ public class MyStorage {
     public static void main(String[] args) {
         // 해당 프로그램의 버전, 점장님의 이름, 프로그램의 기능을 출력합니다.
         //TODO:
-        printProgramInfo();
+        View.printProgramInfo();
         // 위에서 정의한 메서드들을 알맞게 배치하여 줍니다.
         // 사용자의 기능 입력을 받기 위한 Scanner() 메서드를 사용합니다.
         Scanner scanner = new Scanner(System.in);
@@ -29,6 +28,7 @@ public class MyStorage {
         // switch() 를 사용하여 줍니다.
 
         while (true) {
+            View.showMenu();
             int number = selectMenu(scanner);
 
             switch (number) {
@@ -42,52 +42,83 @@ public class MyStorage {
                     prod_amount_add(scanner);
                     break;
                 case 4:
-                    prod_amount_remove(scanner);
+                    prod_amount_decrease(scanner, products);
                     break;
                 case 5:
-                    compute(scanner);
+                    prod_search(scanner, products);
                     break;
                 case 6:
                     exit();
                     break;
                 default:
                     System.out.println("[ERROR] 잘못된 입력입니다.");
-                    continue;
             }
         }
     }
 
-    private static void compute(Scanner scanner) {
-
-    }
-
+    // 6. 프로그램 종료
     private static void exit() {
+        System.out.println(SYSTEM + "프로그램을 종료합니다.");
+        System.out.println(SYSTEM + "감사합니다.");
+        System.exit(0);
+    }
+
+    // 5. 재고 조회
+    private static void prod_search(Scanner scanner, Map<String, Integer> products) {
 
     }
 
-    private static void prod_amount_remove(Scanner scanner) {
+    // 4. 제품 출고
+    private static void prod_amount_decrease(Scanner scanner, Map<String, Integer> products) {
+        System.out.println(SYSTEM + "제품의 출고를 진행합니다.");
+        printProductNamesAndAmount();
 
-    }
-
-    private static void prod_amount_add(Scanner scanner) {
-
-    }
-
-    private static void prod_remove(Scanner scanner) {
-
-    }
-
-    private static void prod_input(Scanner scanner) {
-        System.out.print(SYSTEM + " 제품 등록을 원하는 제품명을 입력하세요 : ");
+        System.out.print(SYSTEM + "출고를 진행할 제품명을 입력하세요 : ");
         String productName = scanner.next();
-        System.out.println();
+        System.out.print(SYSTEM + "원하는 출고량을 입력하세요 : ");
+        int productAmount = scanner.nextInt();
+
+        products.put(productName, products.get(productName) - productAmount);
+        System.out.println(CLEAR + "정상적으로 출고되었습니다.");
+        printProductNamesAndAmount();
+        System.out.println(CONTOUR);
+    }
+
+    // 3. 제품 입고
+    private static void prod_amount_add(Scanner scanner) {
+        System.out.println(SYSTEM + "물건의 수량을 추가합니다.(입고)");
+        printProductNamesAndAmount();
+
+        System.out.print(SYSTEM + "수량을 추가할 제품명을 입력하세요 : ");
+        String productName = scanner.next();
+        System.out.print(SYSTEM + "추가할 수량을 입력하세요 : ");
+        int productAmount = scanner.nextInt();
+
+        products.put(productName, products.get(productName) + productAmount);
+        System.out.println(CLEAR + "정상적으로 제품의 수량 추가가 완료되었습니다.");
+        printProductNamesAndAmount();
+        System.out.println(CONTOUR);
+    }
+
+    // 2. 제품명 등록 취소
+    private static void prod_remove(Scanner scanner) {
+        System.out.print(SYSTEM + "제품 등록 취소를 원하는 제품명을 입력하세요 : ");
+        String productName = scanner.next();
+        products.remove(productName);
+        System.out.println(CLEAR + "제품 취소가 완료되었습니다.");
+    }
+
+    // 1. 제품명 등록
+    private static void prod_input(Scanner scanner) {
+        System.out.print(SYSTEM + "제품 등록을 원하는 제품명을 입력하세요 : ");
+        String productName = scanner.next();
         products.put(productName, 0);
-        System.out.println(SYSTEM + " 등록이 완료되었습니다.");
+        System.out.println(CLEAR + "등록이 완료되었습니다.");
         printProductNames();
     }
 
     private static void printProductNames() {
-        System.out.println(SYSTEM + " 현재 등록된 제품 목록 ▼");
+        System.out.println(SYSTEM + "현재 등록된 제품 목록 ▼");
 
         for (String name : products.keySet()) {
             System.out.println("> " + name);
@@ -101,24 +132,19 @@ public class MyStorage {
         System.out.println(CONTOUR);
     }
 
-    private static void printProgramInfo() {
-        System.out.println(VERSION);
-        System.out.println(CONTOUR);
-        System.out.printf(SYSTEM + " %s 점장님 어서오세요\n", "코드스테이츠");
-        System.out.println(SYSTEM + " 해당 프로그램의 기능입니다.");
-        showMenu();
-    }
+    private static void printProductNamesAndAmount() {
+        System.out.println(SYSTEM + "현재 등록된 제품 및 수량 ▼");
 
-    // showMenu() 메서드가 호출되면 프로그램의 기능들을 보여줄 수 있도록 정의합니다.
-    static void showMenu() {
-        // 사용자를 위해 해당 기능들 앞에 번호를 넣어 알아보기 쉽도록 합니다.
-        System.out.println("1. 물건 정보(제품명) 등록하기");
-        System.out.println("2. 물건 정보(제품명) 등록 취소하기");
-        System.out.println("3. 물건 넣기 (제품 입고)");
-        System.out.println("4. 물건 빼기 (제품 출고)");
-        System.out.println("5. 재고 조회");
-        System.out.println("6. 프로그램 종료");
-        System.out.println(CONTOUR);
+        for (String name : products.keySet()) {
+            System.out.print("> " + name + " : ");
+            System.out.println(products.get(name) + "개");
+        }
+
+        int count = products.size();
+        while (count < REGISTER_QUANTITY) {
+            System.out.println("> 등록 가능 : 0 개");
+            count++;
+        }
     }
 
     // selectMenu() 메서드는 사용자가 프로그램의 기능을 선택할 수 있도록 합니다.
@@ -128,7 +154,7 @@ public class MyStorage {
         //TODO:
         // 프로그램의 기능은 번호(정수)로 전달받습니다.
         // 해당 프로그램은 사용자가 선택한 번호(select)를 반환하도록 합니다.
-        System.out.print(SYSTEM + " 원하는 기능의 번호를 입력하세요 : ");
+        System.out.print(SYSTEM + "원하는 기능의 번호를 입력하세요 : ");
         return scanner.nextInt();
     }
 }
