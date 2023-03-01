565c4418873150b1cc2d0e96894c88212ae0c21c
Isu Shin <isu010614@naver.com>
2023-03-01 11:06:44 +0900

    feat: 사용자 유닛 정보 입력받는 기능 구현

--- src/project/oop_basic/starcraft/Character.java
+++ src/project/oop_basic/starcraft/Character.java
@@ -8,36 +8,57 @@ import java.util.Scanner;
  * @ad :   유닛의 공격력
  * @def :  유닛의 방어력
  * @hp :   유닛의 체력
- * */
+ */
 class Character {
-    //TODO:
-    // 사용자의 입력을 받기위한 객체 생성을 진행합니다.
+    private static final int UNIT_NUMBER = 4;
+    private static final String SYSTEM = "[시스템] 유닛 %s 을 입력해 주세요. : ";
+    private static final String INFO = "[안내] %s\n";
 
+    // 사용자의 입력을 받기위한 객체 생성을 진행합니다.
+    Scanner scanner = new Scanner(System.in);
     // 게임에 필요한 변수 타입을 사전 정의합니다.
+    String name, ad, def, hp;
 
     /**
      * @user_info : 생성된 유닛의 정보를 담는 메서드
      * @this : 해당 메서드가 입력받은 변수들은 this 를
-     * */
+     */
     void user_info(String name, String ad, String def, String hp) {
+        this.name = name;
+        this.ad = ad;
+        this.def = def;
+        this.hp = hp;
     }
 
     /**
      * @user_create() : 유닛을 생성하는 메서드이며, 입력된 값은 user_info() 메서드에게 전달 및 반환 처리합니다.
      * 반환된 값은 시스템 정보창에 출력되도록 합니다.
-     * */
-    String[] user_create() {
-        //TODO:
+     */
+    String[] user_create(boolean isOpponent) {
+        String[] userInfo = new String[UNIT_NUMBER];
+        String[] units = {"[이름]", "[공격력]", "[방어력]", "[체력]"};
         //입력 메시지와 함께, 게임에 필요한 유닛의 정보를 입력받습니다.
+        if (isOpponent) {
+            System.out.printf(INFO, "상대의 유닛 정보를 입력해 주세요.");
+        } else {
+            System.out.printf(INFO, "자신의 유닛 정보를 입력해 주세요.");
+        }
+
+        for (int a = 0; a < UNIT_NUMBER; a++) {
+            System.out.printf(SYSTEM, units[0]);
+            userInfo[a] = scanner.nextLine();
+        }
 
         // 입력된 값은 user_info()에 전달하여 줍니다.
+        this.user_info(userInfo[0], userInfo[1], userInfo[2], userInfo[3]);
 
         // 유저마다의 객체 생성을 위해 반환 과정 또한 정의하여 줍니다.
+        return userInfo;
     }
 
     /**
      * @user_print() : 입력된 배열을 통해 유닛 정보 출력
-     * */
+     */
     void user_print(String[] user) {
         //TODO:
     }
@@ -58,7 +79,7 @@ class Character {
     /**
      * @attack() : 공격을 수행하는 메서드
      * 아군 유닛 정보와 상대의 정보를 입력받습니다.
-     * */
+     */
     void attack(int[] me_info_int, int[] enemy) {
         // 조건 1. 적군의 체력이 0 이하면 [유닛 제거] 가 됩니다.
         // 조건 2. 적군 체력이 0 이하가 아니라면 공격을 성공적으로 수행합니다.
