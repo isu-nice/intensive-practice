af9ce02789c26faad88b8f43b44f1a06432b6f7e
Isu Shin <isu010614@naver.com>
2023-03-04 11:16:46 +0900

    refactor: 주문 객체 DI

--- src/project/oop_intensive/burger_queen/order/Order.java
+++ src/project/oop_intensive/burger_queen/order/Order.java
@@ -1,27 +1,29 @@
 package oop_intensive.burger_queen.order;
 
-import oop_intensive.burger_queen.discount.discountCondition.CozDiscountCondition;
-import oop_intensive.burger_queen.discount.discountCondition.KidDiscountCondition;
+import oop_intensive.burger_queen.discount.discountCondition.DiscountCondition;
+
+import java.util.List;
 
 public class Order {
     private Cart cart;
+    private List<DiscountCondition> discountConditions;
 
-    public Order(Cart cart) {
+    public Order(Cart cart, List<DiscountCondition> discountConditions) {
         this.cart = cart;
+        this.discountConditions = discountConditions;
     }
 
     public void makeOrder() {
-        CozDiscountCondition cozDiscountCondition = new CozDiscountCondition();
-        KidDiscountCondition kidDiscountCondition = new KidDiscountCondition();
-
-        cozDiscountCondition.checkDiscountCondition();
-        kidDiscountCondition.checkDiscountCondition();
 
         int totalPrice = cart.computeTotalPrice();
         int finalPrice = totalPrice;
 
-        if (cozDiscountCondition.isSatisfied()) finalPrice = cozDiscountCondition.applyDiscount(finalPrice);
-        if (kidDiscountCondition.isSatisfied()) finalPrice = kidDiscountCondition.applyDiscount(finalPrice);
+        for (DiscountCondition discountCondition : discountConditions) {
+            discountCondition.checkDiscountCondition();
+            if (discountCondition.isSatisfied()) {
+                finalPrice = discountCondition.applyDiscount(finalPrice);
+            }
+        }
 
         System.out.println("[📣] 주문이 완료되었습니다. ");
         System.out.println("[📣] 주문 내역은 다음과 같습니다. ");
