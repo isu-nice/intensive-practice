8f1a8ac7631b0c438568d8471a57b2387e24b902
Isu Shin <isu010614@naver.com>
2023-03-07 22:40:16 +0900

    feat: 안내 문구

---
+++ src/project/oop_intensive/chicken/view/Guidance.java
@@ -0,0 +1,13 @@
+package oop_intensive.chicken.view;
+
+public class Guidance {
+
+    public static final String CONTOUR = "=".repeat(40);
+    public static final String SYSTEM = "[🔔] ";
+    public static final String INFO = "[📢] ";
+    public static final String INPUT = ">>> ";
+    public static final String STAR = "⭐";
+
+
+}
+
