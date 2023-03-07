c30e527064d95184ad6e849dc7bc04426b567331
Isu Shin <isu010614@naver.com>
2023-03-07 22:39:39 +0900

    feat: main 구현

---
+++ src/project/oop_intensive/chicken/Main.java
@@ -0,0 +1,13 @@
+package oop_intensive.chicken;
+
+public class Main {
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+
+        ChickenNation chickenNation = new ChickenNation(
+                appConfig.registerRestaurant(),
+                appConfig.registerOrder()
+        );
+        chickenNation.start();
+    }
+}
