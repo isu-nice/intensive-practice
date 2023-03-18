2b1f8d4213100bab58511f97d955d1541ea90a13
isu-nice <isu010614@naver.com>
2023-03-18 18:30:13 +0900

    feat: validation 구현

--- src/project/oop_intensive/chicken/order/RegisterOrder.java
+++ src/project/oop_intensive/chicken/order/RegisterOrder.java
@@ -1,14 +1,17 @@
 package oop_intensive.chicken.order;
 
+import oop_intensive.chicken.restaurant.RestaurantRepository;
 import oop_intensive.chicken.view.InputView;
 
 import static oop_intensive.chicken.view.Guidance.INFO;
 
 public class RegisterOrder {
     private OrderRepository orderRepository;
+    private RestaurantRepository restaurantRepository;
 
-    public RegisterOrder(OrderRepository orderRepository) {
+    public RegisterOrder(OrderRepository orderRepository, RestaurantRepository restaurantRepository) {
         this.orderRepository = orderRepository;
+        this.restaurantRepository = restaurantRepository;
     }
 
     public void makeOrder() {
@@ -18,11 +21,11 @@ public class RegisterOrder {
 
         System.out.println(INFO + "주문할 음식점 상호는 무엇인가요?");
         String restaurantName = InputView.getUserInput();
-        // validate
+        validateRestaurantName(restaurantName);
 
         System.out.println(INFO + "주문할 메뉴 이름을 알려주세요!");
         String menuName = InputView.getUserInput();
-        // validate
+        validateMenuName(menuName);
 
         orderRepository.addOrder(
                 new Order(customerName, restaurantName, menuName));
@@ -31,5 +34,21 @@ public class RegisterOrder {
         System.out.printf(INFO + "%s님!\n", customerName);
         System.out.printf(INFO + "%s매장에 %s주문이 완료되었습니다.\n", restaurantName, menuName);
         System.out.println();
+    }
+
+    private void validateRestaurantName(String name) {
+        restaurantRepository.getRestaurants()
+                .stream()
+                .filter(restaurant -> restaurant.getName().equals(name))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private void validateMenuName(String name) {
+        restaurantRepository.getRestaurants()
+                .stream()
+                .filter(restaurant -> restaurant.getMenu().equals(name))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
