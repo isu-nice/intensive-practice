0d6deff2e948259a3209584a37321177884df38f
isu-nice <isu010614@naver.com>
2023-03-18 16:54:49 +0900

    feat: 별점 객체 구현

--- src/project/oop_intensive/chicken/star/Star.java
+++ src/project/oop_intensive/chicken/star/Star.java
@@ -1,7 +1,25 @@
 package oop_intensive.chicken.star;
 
-public class Star {
-    private int number;
-    private String star;
+import oop_intensive.chicken.order.Order;
 
+public class Star {
+    private Order order;
+    private int number;
+
+    public Star(Order order, int number) {
+        this.order = order;
+        this.number = number;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
